2768dd00909792ac750a5a8a637566de2ab4bb32
Simon TRUXILLO <struxill@e2r6p3.42.fr>
2019-04-13 19:21:05 +0200

    Makefile fixed, many protections to do

--- ft_convert_to_t_list.c
+++ ft_convert_to_t_list.c
@@ -6,7 +6,7 @@
 /*   By: struxill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 17:52:18 by struxill          #+#    #+#             */
-/*   Updated: 2019/04/09 14:24:24 by struxill         ###   ########.fr       */
+/*   Updated: 2019/04/13 19:20:32 by struxill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,20 @@ t_list	*ft_convert_to_t_list(char *str)
 
 	i = 1;
 	gap = 21;
-	tmp_str = ft_strsub(str, 0, 19);
-	start = ft_lstnew(tmp_str, i);
+	tmp_str = ft_strsub(str, 0, 19); 	//protect
+	start = ft_lstnew(tmp_str, i);		//protect
 	free(tmp_str);
 	tmp_str = NULL;
 	while (i < (ft_strlen(str) + 1) / 20)
 	{
 		i++;
-		tmp_str = ft_strsub(str, gap, 19);
-		tmp = ft_lstnew(tmp_str, i);
+		tmp_str = ft_strsub(str, gap, 19);  //protect
+		tmp = ft_lstnew(tmp_str, i); 		//protect
 		free(tmp_str);
 		tmp_str = NULL;
 		ft_lstadd(&start, tmp);
+		if (str[gap + 20] == '\0')
+			break ;
 		gap = gap + 21;
 	}
 	return (start);
--- ft_read_input.c
+++ ft_read_input.c
@@ -6,7 +6,7 @@
 /*   By: struxill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 16:20:46 by struxill          #+#    #+#             */
-/*   Updated: 2019/02/12 19:31:13 by struxill         ###   ########.fr       */
+/*   Updated: 2019/04/13 19:11:27 by struxill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@ char	*ft_read_input(char *input)
 	int			fd;
 	static char	buf[BUFF_SIZE + 1];
 
-	fd = open(input, O_RDONLY);
-	ret = read(fd, buf, BUFF_SIZE);
+	if ((fd = open(input, O_RDONLY)) == -1)
+		return (0);
+	if ((ret = read(fd, buf, BUFF_SIZE)) == -1)
+		return (0);
 	buf[ret] = '\0';
-	close(fd);
+	if ((close(fd)) == -1)
+		return (0);
 	return (buf);
 }
