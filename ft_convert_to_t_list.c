7a095880a4a66966eeba6d3b6ee817d296950be1
Simon TRUXILLO <struxill@e3r9p15.42.fr>
2019-04-22 15:30:23 +0200

    ft_convert_to_t_list now fully protected

--- ft_convert_to_t_list.c
+++ ft_convert_to_t_list.c
@@ -6,33 +6,65 @@
 /*   By: struxill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 17:52:18 by struxill          #+#    #+#             */
-/*   Updated: 2019/04/13 19:20:32 by struxill         ###   ########.fr       */
+/*   Updated: 2019/04/22 15:29:39 by struxill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+t_list	*ft_t_list_start(char *str)
+{
+	t_list	*start;
+	char	*tmp_str;
+
+	if (!(tmp_str = ft_strsub(str, 0, 19)))
+		return (NULL);
+	if (!(start = ft_lstnew(tmp_str, 1)))
+	{
+		free(tmp_str);
+		tmp_str = NULL;
+		return (NULL);
+	}
+	free(tmp_str);
+	tmp_str = NULL;
+	return (start);
+}
+
+t_list	*ft_t_list_tmp(char *str, size_t i, int gap)
+{
+	t_list	*tmp;
+	char	*tmp_str;
+
+	if (!(tmp_str = ft_strsub(str, gap, 19)))
+		return (NULL);
+	if (!(tmp = ft_lstnew(tmp_str, i)))
+	{
+		free(tmp_str);
+		tmp_str = NULL;
+		return (NULL);
+	}
+	free(tmp_str);
+	tmp_str = NULL;
+	return (tmp);
+}
 
 t_list	*ft_convert_to_t_list(char *str)
 {
 	t_list	*start;
 	t_list	*tmp;
 	size_t	i;
-	char	*tmp_str;
 	int		gap;
 
 	i = 1;
 	gap = 21;
-	tmp_str = ft_strsub(str, 0, 19); 	//protect
-	start = ft_lstnew(tmp_str, i);		//protect
-	free(tmp_str);
-	tmp_str = NULL;
-	while (i < (ft_strlen(str) + 1) / 20)
+	start = ft_t_list_start(str);
+	while (i++ < (ft_strlen(str) + 1) / 20)
 	{
-		i++;
-		tmp_str = ft_strsub(str, gap, 19);  //protect
-		tmp = ft_lstnew(tmp_str, i); 		//protect
-		free(tmp_str);
-		tmp_str = NULL;
+		if (!(tmp = ft_t_list_tmp(str, i, gap)))
+		{
+			ft_lstfree(start);
+			return (NULL);
+		}
 		ft_lstadd(&start, tmp);
 		if (str[gap + 20] == '\0')
 			break ;
@@ -40,33 +72,3 @@ t_list	*ft_convert_to_t_list(char *str)
 	}
 	return (start);
 }
-
-/*
-t_list	*ft_convert_to_t_list(char *str)
-{
-	t_list	*start;
-	t_list	*tmp;
-	size_t	i;
-	char	*tmp_str;
-	int		gap;
-
-	i = 1;
-	gap = 21;
-	tmp_str = ft_strsub(str, 0, 19); 	//protect
-	start = ft_lstnew(tmp_str, i);		//protect
-	free(tmp_str);
-	tmp_str = NULL;
-	while (i < (ft_strlen(str) + 1) / 20)
-	{
-		i++;
-		tmp_str = ft_strsub(str, gap, 19);  //protect
-		tmp = ft_lstnew(tmp_str, i); 		//protect
-		free(tmp_str);
-		tmp_str = NULL;
-		ft_lstadd(&start, tmp);
-		if (str[gap + 20] == '\0')
-			break ;
-		gap = gap + 21;
-	}
-	return (start);
-} */
