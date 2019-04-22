886ee097f93ddb1b4092fd317be608cfc6ff0ebc
Simon TRUXILLO <struxill@e3r9p15.42.fr>
2019-04-22 15:32:08 +0200

    ajout du nouveau create tab et du test a retry

--- ft_create_tab.c
+++ ft_create_tab.c
@@ -6,7 +6,7 @@
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 12:15:00 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/04/13 19:12:08 by struxill         ###   ########.fr       */
+/*   Updated: 2019/04/09 14:23:58 by struxill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ char	**ft_create_tab(int size)
 	i = 0;
 	if (!(tab = (char**)malloc(sizeof(char*) * (size + 1))))
 		return (NULL);
-	s1 = ft_strnew(size); //protect
-	ft_memset(s1, '.', size); //protect
+	s1 = ft_strnew(size);
+	ft_memset(s1, '.', size);
 	while (i < size)
 	{
-		tab[i] = ft_strdup(s1); //protect
+		tab[i] = ft_strdup(s1);
 		i++;
 	}
 	free(s1);
